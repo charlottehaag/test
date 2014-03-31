e5aa128af7d7aae827b0eb301f5b8a05e32ad04b
charlottehaag <haag.char@gmail.com>
2014-03-31 13:44:30 +0200

    Signed-off-by: charlottehaag <haag.char@gmail.com>

--- PPD-FINAL/vue/pagePrincipale.php
+++ PPD-FINAL/vue/pagePrincipale.php
@@ -2,7 +2,7 @@
 <html>
     <head>
 
-        <title>D&eacute;limiam</title>
+        <title>D&eacute;limiamzhjegdfjzg</title>
 
 		<!--<link rel="stylesheet" type="text/css" href="commun/style/stylePage.css" />-->
 
