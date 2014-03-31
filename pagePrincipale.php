5543e1e0b841e6e8452316f1cfa2b4fb6a3e0bbc
charlottehaag <haag.char@gmail.com>
2014-03-31 14:04:15 +0200

    Rechangement de titre new Signed-off-by: charlottehaag <haag.char@gmail.com>

--- pagePrincipale.php
+++ pagePrincipale.php
@@ -2,7 +2,7 @@
 <html>
     <head>
 
-        <title>D&eacute;limiamMiam</title>
+        <title>D&eacute;limiamMiamMiam</title>
 
 		<!--<link rel="stylesheet" type="text/css" href="commun/style/stylePage.css" />-->
 
