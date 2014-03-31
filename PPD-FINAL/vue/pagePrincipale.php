b8e7eb4ab1073b8ff8a649699de87d2d5d8ddf0a
charlottehaag <haag.char@gmail.com>
2014-03-31 13:38:07 +0200

    second commit

--- PPD-FINAL/vue/pagePrincipale.php
+++ PPD-FINAL/vue/pagePrincipale.php
@@ -2,7 +2,7 @@
 <html>
     <head>
 
-        <title>D&eacute;limiamMiamdezedz</title>
+        <title>D&eacute;limiam</title>
 
 		<!--<link rel="stylesheet" type="text/css" href="commun/style/stylePage.css" />-->
 
